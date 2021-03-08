d79ec8b5fe9a11d71e0f5db171df59cb07798d9e
hobee_mac <lzqpublic@163.com>
2021-03-08 21:26:37 +0800

    core code complete

--- src/react/main_window.js
+++ src/react/main_window.js
@@ -1,4 +1,42 @@
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.md = new Remarkable();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'Hello, **world**! hi' };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  getRawMarkup() {
+    return { __html: this.md.render(this.state.value) };
+  }
+
+  render() {
+    return (
+      <div className="MarkdownEditor">
+        <h3>Input</h3>
+        <label htmlFor="markdown-content">
+          Enter some markdown
+        </label>
+        <textarea
+          id="markdown-content"
+          onChange={this.handleChange}
+          defaultValue={this.state.value}
+        />
+        <h3>Output</h3>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={this.getRawMarkup()}
+        />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-    <h1> hello,world! </h1>,
-    document.getElementById('main_window')
-)
+  <MarkdownEditor />,
+  document.getElementById('main_window')
+);
